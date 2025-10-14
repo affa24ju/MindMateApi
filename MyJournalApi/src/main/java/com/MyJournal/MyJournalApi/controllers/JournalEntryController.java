68cf482a25763f6c623d2ab5f5bf3ce4add96e67
Fatema Afroza <fafroza2017@gmail.com>
2025-10-14 10:02:33 +0200

    La till /deleteJournalEntry endpoint

--- MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/controllers/JournalEntryController.java
+++ MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/controllers/JournalEntryController.java
@@ -5,8 +5,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -89,6 +92,12 @@ public class JournalEntryController {
     }
 
     // Endpoint för att redera ett journal inlägg
+    @DeleteMapping("/deleteJournalEntry/{entryId}")
+    public ResponseEntity<String> deleteJournalEntry(@PathVariable String entryId) {
+        User currentUser = userService.getCurrentUser();
+        journalEntryService.deleteJournalEntry(entryId, currentUser);
+        return ResponseEntity.ok().body("Inlägget har raderats!");
+    }
 
     // Endpoint för att uppdatera ett inlägg
 
