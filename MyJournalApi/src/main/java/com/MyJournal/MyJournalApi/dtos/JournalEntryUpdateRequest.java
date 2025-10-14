0b83d825251e7b15b14f433a656deeb617a79c6a
Fatema Afroza <fafroza2017@gmail.com>
2025-10-14 10:14:31 +0200

    Dto till updateRequest

---
+++ MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/dtos/JournalEntryUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.MyJournal.MyJournalApi.dtos;
+
+import java.time.LocalDateTime;
+
+import com.MyJournal.MyJournalApi.models.Feeling;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JournalEntryUpdateRequest {
+    // Dto för att uppdatera inlägg
+    private String note;
+    private Feeling feeling;
+    private LocalDateTime updatedAt;
+
+}
