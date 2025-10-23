4fa5e3e787a39f3518561a530be27208fd5bf497
Fatema Afroza <fafroza2017@gmail.com>
2025-10-23 11:17:52 +0200

    Skapat JournalStatsServiceTest

---
+++ MyJournalApi/src/test/java/com/MyJournal/MyJournalApi/services/JournalStatsServiceTest.java
@@ -0,0 +1,10 @@
+package com.MyJournal.MyJournalApi.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+// @ExtendWith - Använder MockitoExtension för att möjliggöra Mockito-funktioner i JUnit 5 tester
+@ExtendWith(MockitoExtension.class)
+public class JournalStatsServiceTest {
+
+}
