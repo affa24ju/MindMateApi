c5c61162325f78387e863c99638bb5baa93923bb
Fatema Afroza <fafroza2017@gmail.com>
2025-10-23 11:49:21 +0200

    Test för att få rätt räckning & procent

--- MyJournalApi/src/test/java/com/MyJournal/MyJournalApi/services/JournalStatsServiceTest.java
+++ MyJournalApi/src/test/java/com/MyJournal/MyJournalApi/services/JournalStatsServiceTest.java
@@ -1,10 +1,53 @@
 package com.MyJournal.MyJournalApi.services;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-// @ExtendWith - Använder MockitoExtension för att möjliggöra Mockito-funktioner i JUnit 5 tester
-@ExtendWith(MockitoExtension.class)
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.MyJournal.MyJournalApi.dtos.JournalStatsResponse;
+import com.MyJournal.MyJournalApi.models.Feeling;
+import com.MyJournal.MyJournalApi.models.JournalEntry;
+
 public class JournalStatsServiceTest {
+
+    private JournalStatsService journalStatsService;
+
+    @BeforeEach
+    void setUp() {
+        journalStatsService = new JournalStatsService();
+    }
+
+    // Test för getStats-metoden
+    @Test
+    void testGetStats_ShouldReturnCorrectCountAndPercentages() {
+        // Arrange
+        List<JournalEntry> entries = List.of(
+                JournalEntry.builder().feeling(Feeling.GLAD).build(),
+                JournalEntry.builder().feeling(Feeling.TIRED).build(),
+                JournalEntry.builder().feeling(Feeling.GLAD).build());
+        // Act
+        JournalStatsResponse result = journalStatsService.getStats(entries);
+
+        // Assert
+        // Kollar att resultatet inte är null, returnerar korrekt antal och procent
+        assertNotNull(result);
+        assertEquals(3, result.getTotalEntries());
+
+        // Kollar att räkningen och procenten för varje känsla är korrekt
+        Map<Feeling, Long> counts = result.getFeelingCounts();
+        assertEquals(2L, counts.get(Feeling.GLAD));
+        assertEquals(1L, counts.get(Feeling.TIRED));
+
+        // Kollar procenten
+        Map<Feeling, Double> percentages = result.getFeelingPercentages();
+        assertEquals(66.666, percentages.get(Feeling.GLAD), 0.01);
+        assertEquals(33.333, percentages.get(Feeling.TIRED), 0.01);
+
+    }
 
 }
