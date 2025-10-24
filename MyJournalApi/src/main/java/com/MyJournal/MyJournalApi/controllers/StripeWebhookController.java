f439a123410433c782affd8e20f5fa486a713d11
Fatema Afroza <fafroza2017@gmail.com>
2025-10-24 09:35:37 +0200

    La till @PostMapping(/webhook), inte färdigt än

--- MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/controllers/StripeWebhookController.java
+++ MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/controllers/StripeWebhookController.java
@@ -1,12 +1,50 @@
 package com.MyJournal.MyJournalApi.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.MyJournal.MyJournalApi.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.model.Event;
+import com.stripe.net.Webhook;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/myJournal/payments")
 @CrossOrigin(origins = "http://localhost:4200") // Allow requests from Angular dev server
+@RequiredArgsConstructor
 public class StripeWebhookController {
+
+    @Value("${stripe.webhook.secret}")
+    private String endpointSecret;
+
+    private final UserService userService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/webhook")
+    public ResponseEntity<String> handleStripeWebhook(@RequestBody String payload,
+            @RequestHeader("Stripe-Signature") String sigHeader) {
+        // Event från Stripe
+        Event event;
+        try {
+            event = Webhook.constructEvent(
+                    payload, sigHeader, endpointSecret);
+        } catch (SignatureVerificationException e) {
+            // Signaturen är ogiltig
+            return ResponseEntity.badRequest().body("Invalid signature");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error parsing webhook event");
+        }
+        // Svarar med 200 OK för att stripe vet att jag mottagit eventet
+        return ResponseEntity.ok("Webhook received");
+    }
 
 }
